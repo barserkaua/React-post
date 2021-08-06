c71351a55c55b6b7a106fc84a51c06232917bf3e
barserka <barserkaua@gmail.com>
2021-08-06 22:05:41 +0300

    add site structure

---
+++ src/components/post-status-filter/post-status-filter.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const PostStatusFilter = () => {
+    return (
+        <div className="btn-group">
+            <button className="btn btn-outline-primary">
+                All
+            </button>
+            <button/>
+        </div>
+    )
+}
+
+export default PostStatusFilter;
