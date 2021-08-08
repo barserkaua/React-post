2366705c03b17045982a6c03ee59745fd629eacc
barserka <barserkaua@gmail.com>
2021-08-08 21:14:23 +0300

    build function to delete element and add element

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,13 +1,31 @@
 import React from "react";
 
 import './app-header.css'
+import styled from 'styled-components';
+
+const Header = styled.div`
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+    h1{
+        font-size: 26px;
+        color: ${props => props.colored ? 'red' : 'black'};
+        :hover {
+            color: green;
+        }
+    }
+    h2{
+         font-size: 1.2rem;
+         color: grey;
+    }
+`
 
 const AppHeader = () => {
     return (
-        <div className="app-header d-flex">
+        <Header as='a'>
             <h1>Vasyl Perehinets</h1>
             <h2>5 records, of which I liked 0</h2>
-        </div>
+        </Header>
     )
 }
 
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 
 import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
@@ -8,26 +8,76 @@ import PostAddForm from "../post-add-form";
 
 import './app.css';
 
-const App = () => {
+import styled from 'styled-components';
 
-    const data = [
-        {label: 'Going to learn React', important: true, id: 'qss'},
-        {label: 'That is so good', important: false, id: 'dasd'},
-        {label: 'I need a break...', important: false, id: 'adgg'}
+const AppBlock = styled.div`
+  margin: 0 auto;
+  max-width: 800px;
+`;
 
-    ];
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data : [
+                {label: 'Going to learn React', important: true, id: 1},
+                {label: 'That is so good', important: false, id: 2},
+                {label: 'I need a break...', important: false, id: 3}
 
-    return (
-        <div className="app">
-            <AppHeader/>
-            <div className="search-panel d-flex">
-                <SearchPanel/>
-                <PostStatusFilter/>
-            </div>
-            <PostList posts={data}/>
-            <PostAddForm/>
-        </div>
-    );
+            ]
+        };
+        this.deleteItem = this.deleteItem.bind(this);
+        this.addItem = this.addItem.bind(this);
+
+        this.maxId = 4;
+
+    }
+
+    deleteItem(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+
+            const before = data.slice(0, index);
+            const after = data.slice(index + 1);
+
+            const newArr = [...before, ...after];
+
+            return{
+                data: newArr
+            }
+        });
+    }
+
+    addItem(body) {
+        const newItem = {
+            label: body,
+            important: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    render() {
+        return (
+            <AppBlock>
+                <AppHeader/>
+                <div className="search-panel d-flex">
+                    <SearchPanel/>
+                    <PostStatusFilter/>
+                </div>
+                <PostList
+                    posts={this.state.data}
+                    onDelete={this.deleteItem}/>
+                <PostAddForm
+                    onAdd={this.addItem}/>
+            </AppBlock>
+        );
+    }
+
+
 }
-
-export default App;
